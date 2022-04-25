bc40d5ab6e417028c47af457d737ee54617c686e
anhak777 <annahakobyan2889@gmail.com>
2022-04-25 15:50:17 +0400

    updated routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -12,7 +12,7 @@ export class RecipeService {
         new Recipe(
             'Tasty Schnitzel', 
             'A super-tasty Schnitzel - just awesome!', 
-            'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
+            '../../../assets/pexels-photo-schnitzel.jpeg',
             [
                 new Ingredient('Meat', 1),
                 new Ingredient('French Fries', 20)
@@ -20,12 +20,12 @@ export class RecipeService {
         new Recipe(
             'Big Fat Burger', 
             'What else you need to say?', 
-            'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
+            '../../../assets/pexels-photo-burger.jpeg',
             [
                 new Ingredient('Buns', 2),
                 new Ingredient('Meat', 1),
                 new Ingredient('Cheese', 1),
-                new Ingredient('Tomato', 2)
+                new Ingredient('Tomato', 1)
             ])
     ];
 
@@ -33,6 +33,10 @@ export class RecipeService {
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    getRecipe(index: number) {
+        return this.recipes[index];
     }
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
