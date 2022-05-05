ac324a061281ac226825f6168ca1d71e1a4d4603
anna.hakobyan-at-291959568122 <annahakobyan2889@gmail.com>
2022-05-05 18:49:39 +0400

    new

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,63 +1,70 @@
-import { Injectable } from "@angular/core";
-import { Subject } from "rxjs/internal/Subject";
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs/internal/Subject';
 
-import { Ingredient } from "../shared/ingredient.model";
-import { ShoppingListService } from "../shopping-list/shopping-list.service";
-import { Recipe } from "./recipe.model";
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
-    recipesChanged = new Subject<Recipe[]>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Tasty Schnitzel',
+      'A super-tasty Schnitzel - just awesome!',
+      '../../../assets/pexels-photo-schnitzel.jpeg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'Big Fat Burger',
+      'What else you need to say?',
+      '../../../assets/pexels-photo-burger.jpeg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Cheese', 1),
+        new Ingredient('Tomato', 1),
+      ]
+    ),
+  ];
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'Tasty Schnitzel', 
-            'A super-tasty Schnitzel - just awesome!', 
-            '../../../assets/pexels-photo-schnitzel.jpeg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe(
-            'Big Fat Burger', 
-            'What else you need to say?', 
-            '../../../assets/pexels-photo-burger.jpeg',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1),
-                new Ingredient('Cheese', 1),
-                new Ingredient('Tomato', 1)
-            ])
-    ];
+  constructor(private slService: ShoppingListService) {}
 
-    constructor(private slService: ShoppingListService) {}
+  getRecipes() {
+    return this.recipes.slice();
+  }
 
-    getRecipes() {
-        return this.recipes.slice();
-    }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
 
-    getRecipe(index: number) {
-        return this.recipes[index];
-    }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+  /**
+   * @param  {Recipe} recipe
+   *
+   * * This is an important text
+   * ! Alert comment
+   * ? idk , some question
+   * TODO: remember this
+   *
+   *
+   * this is a link to other function, just hover over this funcName {@link getRecipe}
+   */
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
-    addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.slService.addIngredients(ingredients);
-    }
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
-    addRecipe(recipe: Recipe) {
-        this.recipes.push(recipe);
-        this.recipesChanged.next(this.recipes.slice());
-    }
-
-    updateRecipe(index: number, newRecipe: Recipe) {
-        this.recipes[index] = newRecipe;
-        this.recipesChanged.next(this.recipes.slice());
-    }
-
-    deleteRecipe(index: number) {
-        this.recipes.splice(index, 1);
-        this.recipesChanged.next(this.recipes.slice());
-    }
-
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
+  }
 }
