fb6a3b13f38803da59d8c31666fe485a11fe4fb3
anna.hakobyan <anna.hakobyan@applebrie.com>
2022-05-18 18:52:40 +0400

    added animation and more recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -14,7 +14,36 @@ export class RecipeService {
       'Tasty Schnitzel',
       'A super-tasty Schnitzel - just awesome!',
       '../../../assets/pexels-photo-schnitzel.jpeg',
-      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+      [
+        new Ingredient('Boneless pork chops', 2),
+        new Ingredient('All-purpose flour', 1/25),
+        new Ingredient('Garlic salt', 1),
+        new Ingredient('Paprika', 1/2),
+        new Ingredient('Black pepper', 1/2),
+        new Ingredient('Large eggs', 3),
+        new Ingredient('Panko bread crumbs', 2),
+        new Ingredient('Olive oil', 1),
+        new Ingredient('Lemon wedges', 3),
+        new Ingredient('French Fries', 20),
+        new Ingredient('Ketchup', 1),
+        new Ingredient('Marole', 1)
+      ]
+    ),
+    new Recipe(
+      'Baked Salmon',
+      'Baked Salmon with Garlic is juicy and flaky, yum!',
+      '../../../assets/Baked-Salmon.jpg',
+      [
+        new Ingredient('1/2 lb salmon filet', 1),
+        new Ingredient('Fresh parsley, chopped', 2),
+        new Ingredient('Light olive oil', 2),
+        new Ingredient('Fresh lemon juice', 2),
+        new Ingredient('Garlic cloves, pressed', 3),
+        new Ingredient('Dijon mustard', 1/2),
+        new Ingredient('Sea salt', 1/2),
+        new Ingredient('Black pepper', 1/8),
+        new Ingredient('Lemon, sliced into 4 rings', 1/2),
+      ]
     ),
     new Recipe(
       'Big Fat Burger',
@@ -27,6 +56,7 @@ export class RecipeService {
         new Ingredient('Tomato', 1),
       ]
     ),
+
   ];
 
   constructor(private slService: ShoppingListService) {}
